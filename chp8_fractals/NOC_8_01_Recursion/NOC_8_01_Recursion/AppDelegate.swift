afc24b3741928a853029b874ea68771de67f531a
Loren Olson <loren.olson@asu.edu>
2017-09-28 16:55:37 -0700

    Add Chapter 8, Example 01

---
+++ chp8_fractals/NOC_8_01_Recursion/NOC_8_01_Recursion/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NOC_8_01_Recursion
+//
+//  Created by Loren Olson on 9/28/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ chp8_fractals/NOC_8_01_Recursion/NOC_8_01_Recursion/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  NOC_8_01_Recursion
+//
+//
+
+import Cocoa
+import Tin
+
+class ViewController: TController {
+
+    override func viewWillAppear() {
+        view.window?.title = "Recursion"
+        makeView(width: 640.0, height: 360.0)
+        let scene = Scene()
+        present(scene: scene)
+        scene.view?.showStats = false
+    }
+
+}
+
+
+class Scene: TScene {
+    
+    override func update() {
+        background(gray: 1.0)
+        
+        drawCircle(x: tin.midX, y: tin.midY, r: tin.width)
+        view?.stopUpdates()
+    }
+    
+    
+    func drawCircle(x: Double, y: Double, r: Double) {
+        ellipse(centerX: x, centerY: y, width: r, height: r)
+        if r > 2.0 {
+            let smaller = r * 0.75
+            drawCircle(x: x, y: y, r: smaller)
+        }
+    }
+    
+}
+

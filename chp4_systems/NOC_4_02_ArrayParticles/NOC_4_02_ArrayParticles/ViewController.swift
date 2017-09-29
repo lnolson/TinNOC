a1ee79875b6b5aeffd8bc8743c81fbb56161c926
Loren Olson <loren.olson@asu.edu>
2017-09-28 19:52:07 -0700

    Add Chapter 4, Example 2. Array Particles.

---
+++ chp4_systems/NOC_4_02_ArrayParticles/NOC_4_02_ArrayParticles/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  NOC_8_01_Recursion
+//
+//
+//
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+import Cocoa
+import Tin
+
+class ViewController: TController {
+
+    override func viewWillAppear() {
+        view.window?.title = "Array of Particles"
+        makeView(width: 640.0, height: 360.0)
+        let scene = Scene()
+        present(scene: scene)
+        scene.view?.showStats = false
+    }
+
+}
+
+
+class Scene: TScene {
+    
+    var particles: [Particle] = [Particle]()
+    
+    
+    override func setup() {
+    }
+    
+    override func update() {
+        background(gray: 1.0)
+        
+        let p = Particle(location: TVector2(x: tin.midX, y: tin.height - 50.0))
+        particles.insert(p, at: 0)
+        
+        for p in particles {
+            p.run()
+            if p.isDead() {
+                if let i = particles.index(of: p) {
+                    particles.remove(at: i)
+                }
+            }
+        }
+    
+    }
+    
+}
+

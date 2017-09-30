9a93ab2a894efaf6c1a24a0315d75a6faecb78cc
Loren Olson <loren.olson@asu.edu>
2017-09-29 20:55:47 -0700

    Add Chapter 4, Example 3. Particle System Class.

---
+++ chp4_systems/NOC_4_03_ParticleSystemClass/NOC_4_03_ParticleSystemClass/ParticleSystem.swift
@@ -0,0 +1,40 @@
+//
+//  ParticleSystem.swift
+//  NOC_4_03_ParticleSystemClass
+//
+//
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+import Foundation
+import Tin
+
+
+class ParticleSystem {
+    var particles: [Particle]
+    var origin: TVector2
+    
+    init(position: TVector2) {
+        origin = position
+        particles = []
+    }
+    
+    
+    func addParticle() {
+        let p = Particle(location: origin)
+        particles.insert(p, at: 0)
+    }
+    
+    
+    func run() {
+        for p in particles {
+            p.run()
+            if p.isDead() {
+                if let i = particles.index(of: p) {
+                    particles.remove(at: i)
+                }
+            }
+        }
+    }
+}
---
+++ chp4_systems/NOC_4_03_ParticleSystemClass/NOC_4_03_ParticleSystemClass/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  NOC_8_01_Recursion
+//
+//
+//
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+import Cocoa
+import Tin
+
+class ViewController: TController {
+
+    override func viewWillAppear() {
+        view.window?.title = "Particle System Class"
+        makeView(width: 640.0, height: 360.0)
+        let scene = Scene()
+        present(scene: scene)
+        scene.view?.showStats = false
+    }
+
+}
+
+
+class Scene: TScene {
+    
+    var ps: ParticleSystem!
+    
+    
+    override func setup() {
+        ps = ParticleSystem(position: TVector2(x: tin.midX, y: tin.height - 50.0))
+    }
+    
+    override func update() {
+        background(gray: 1.0)
+        
+        
+        ps.addParticle()
+        ps.run()
+    
+    }
+    
+}
+

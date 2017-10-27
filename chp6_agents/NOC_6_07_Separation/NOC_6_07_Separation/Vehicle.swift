e2f93761ab639f2df7c71687dc5d63d41a5105b1
Loren Olson <loren.olson@asu.edu>
2017-10-27 14:10:53 -0700

    Add Chapter 6, Example 7 Separation.

---
+++ chp6_agents/NOC_6_07_Separation/NOC_6_07_Separation/Vehicle.swift
@@ -0,0 +1,134 @@
+//
+//  Vehicle.swift
+//  NOC_6_07_Separation
+//
+//
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Vehicle class
+
+import Foundation
+import Tin
+
+class Vehicle {
+    var position: TVector2
+    var velocity: TVector2
+    var acceleration: TVector2
+    var r: Double
+    var maxforce: Double
+    var maxspeed: Double
+    
+    
+    init(x: Double, y: Double) {
+        acceleration = TVector2()
+        velocity = TVector2()
+        position = TVector2(x: x, y: y)
+        r = 12.0
+        maxspeed = 3.0
+        maxforce = 0.2
+    }
+    
+    
+    func update() {
+        velocity = velocity + acceleration
+        velocity.limit(mag: maxspeed)
+        position = position + velocity
+        acceleration = acceleration * 0.0
+    }
+    
+    
+    func apply(force: TVector2) {
+        acceleration = acceleration + force
+    }
+    
+    
+    
+    // Separation
+    // Method checks for nearby vehicles and steers away
+    func separate(vehicles: [Vehicle]) {
+        let desiredSeparation = r * 2.0
+        var sum = TVector2()
+        var count = 0
+        // For every boid in the system, check if it's too close
+        for other in vehicles {
+            let d = position.distance(v: other.position)
+            if d > 0.0 && d < desiredSeparation {
+                var diff = position - other.position
+                diff.normalize()
+                diff = diff / d         // Weight by distance
+                sum = sum + diff
+                count += 1             // Keep track of how many
+            }
+        }
+        
+        // Average -- divide by how many
+        if count > 0 {
+            // Our desired vector is moving away maximum speed
+            sum.magnitude = maxspeed
+            // Implement Reynolds: Steering = Desired - Velocity
+            var steer = sum - velocity
+            steer.limit(mag: maxforce)
+            apply(force: steer)
+        }
+    }
+    
+    
+    func seek(target: TVector2) {
+        var desired = target - position
+        desired.normalize()
+        desired = desired * maxspeed
+        
+        var steer = desired - velocity
+        steer.limit(mag: maxforce)
+        
+        apply(force: steer)
+    }
+    
+    // A method that calculates a steering force towards a target
+    // STEER = DESIRED MINUS VELOCITY
+    func arrive(target: TVector2) {
+        var desired = target - position
+        let d = desired.magnitude
+        if d < 100 {
+            let m = remap(value: d, start1: 0, stop1: 100, start2: 0, stop2: maxspeed)
+            desired.magnitude = m
+        }
+        else {
+            desired.magnitude = maxspeed
+        }
+        
+        var steer = desired - velocity
+        steer.limit(mag: maxforce)
+        apply(force: steer)
+    }
+    
+    
+    func display() {
+        fillColor(gray: 0.6862)
+        strokeColor(gray: 0)
+        lineWidth(1.0)
+        pushState()
+        translate(dx: position.x, dy: position.y)
+        ellipse(centerX: 0, centerY: 0, width: r, height: r)
+        popState()
+    }
+    
+    
+    func borders() {
+        if position.x < -r {
+            position.x = tin.width + r
+        }
+        if position.y < -r {
+            position.y = tin.height + r
+        }
+        if position.x > tin.width + r {
+            position.x = -r
+        }
+        if position.y > tin.height + r {
+            position.y = -r
+        }
+    }
+}
+
---
+++ chp6_agents/NOC_6_07_Separation/NOC_6_07_Separation/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  NOC_6_07_Separation
+//
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+
+// Separation
+// Via Reynolds: http://www.red3d.com/cwr/steer/
+
+import Cocoa
+import Tin
+
+class ViewController: TController {
+
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+        view.window?.title = "Separation"
+        
+        makeView(width: 640.0, height: 360.0)
+        let scene = Scene()
+        present(scene: scene)
+        
+        scene.view?.showStats = false
+    }
+
+}
+
+
+class Scene: TScene {
+    
+    var vehicles: [Vehicle] = []
+    var font = TFont(fontName: "Helvetica Neue Light", ofSize: 14.0)
+    
+    override func setup() {
+        for _ in 1...100 {
+            let x = random(max: tin.width)
+            let y = random(max: tin.height)
+            let v = Vehicle(x: x, y: y)
+            vehicles.append(v)
+        }
+        font.horizontalAlignment = .left
+    }
+    
+    
+    override func update() {
+        background(gray: 1.0)
+        
+        for v in vehicles {
+            v.separate(vehicles: vehicles)
+            v.update()
+            v.borders()
+            v.display()
+        }
+        
+        fillColor(gray: 0)
+        text(message: "Drag the mouse to generate new vehicles", font: font, x: 10.0, y: 10.0)
+    }
+}
+
